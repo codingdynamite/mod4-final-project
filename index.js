87ad73e8ec435ba6867b737463c18a6540c3c95d
Joshua Mason <thelementalcode@gmail.com>
2025-05-06 19:38:33 -0700

    js issue

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-// Titles: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
-// details: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
+// Titles: https://omdbapi.com/?s=thor&page=1&apikey=5489aa38
+// details: http://www.omdbapi.com/?i=tt3896198&apikey=5489aa38
+
+const movieSearchBox = document.getElementById('movie__search--box');
+const searchList = document.getElementById('search__list');
+const resultGrid = document. getElementById('result__grid');
+
+/* LOAD MOVIES FROM API */
+
+async function loadMovies(searchTerm) {
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=5489aa38`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    // console.log(data.Search);
+    if (data.Response === 'True') displayMovieList(data.Search);
+}
+
+function findMovies() {
+    let searchTerm = (movieSearchBox.value);
+    
+}
+
+function displayMovieList(movies) {
+
+}
